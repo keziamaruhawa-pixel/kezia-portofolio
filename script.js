fa8bfc7ace0eea68d096977c9420f6680f51fb98
ziaa ✨ <keziamaruhawa@gmail.com>
2026-03-09 16:16:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function sayHi() {
+  alert("Hello! Thanks for visiting my portfolio 😊");
+}
+
+// scroll reveal
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
